3f5e8d6234607a40a9ed199c0376b5db2ec049a9
Mohammed Usman <imailusman07@gmail.com>
2025-08-06 15:55:25 +0530

    Register models with Django admin interface

---
+++ App/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from App.models import *
+
+# Register your models here.
+admin.site.register(HR)
+admin.site.register(Instructor)
+admin.site.register(Department)
+admin.site.register(Activities)
+admin.site.register(Capital)
